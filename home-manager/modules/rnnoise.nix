47272d1cb8a2e1b646ebb80a86cec3ca7bf16cf8
DiegoVG591 <diegovg591@gmail.com>
2025-07-07 01:49:28 +0200

    Trying a diferent noise supresor algorithm

--- home-manager/modules/rnnoise.nix
+++ home-manager/modules/rnnoise.nix
@@ -1,40 +1,36 @@
+# In /home/krieg/mysystem/home-manager/modules/rnnoise.nix
 { pkgs, ... }:
 
 {
-  # This creates the config file in the correct directory
   home.file.".config/pipewire/pipewire.conf.d/99-input-denoising.conf" = {
     text = ''
       context.modules = [
         {
           name = libpipewire-module-filter-chain
           args = {
-            node.description = "Noise Canceling Microphone"
-            media.name       = "Noise Canceling Microphone"
+            node.description = "Denoised Blue Yeti"
+            media.name       = "Denoised Blue Yeti"
             node.target      = "alsa_input.usb-Generic_Blue_Microphones_2112BAB09868-00.analog-stereo"
+            capture.props = {
+              node.name   = "capture.denoised_yeti"
+              audio.rate  = 48000
+            }
+            playback.props = {
+              node.name   = "denoised_yeti"
+              media.class = "Audio/Source"
+            }
             filter.graph = {
               nodes = [
                 {
-                  type  = ladspa
-                  name  = rnnoise
-                  plugin = ${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so
-                  label = noise_suppressor_mono
+                  type    = "builtin"
+                  name    = "denoiser"
+                  label   = "noise_suppressor_stereo"
                   control = {
-                    "VAD Threshold (%)" = 50.0
-                    "VAD Grace Period (ms)" = 200
-                    "Retroactive VAD Grace (ms)" = 0
+                    # This controls the STRENGTH. Start low and increase if needed.
+                    "noise.suppress_ms" = 10
                   }
                 }
               ]
-            }
-            capture.props = {
-              node.name = "capture.rnnoise_source"
-              node.passive = true
-              audio.rate = 48000
-            }
-            playback.props = {
-              node.name = "rnnoise_source"
-              media.class = Audio/Source
-              audio.rate = 48000
             }
           }
         }
@@ -42,7 +38,7 @@
     '';
   };
 
-  home.packages = [
-    pkgs.rnnoise-plugin
-  ];
+  # The rnnoise-plugin is no longer needed with this method
+  # but we leave the line here and comment it out for future reference.
+  # home.packages = [ pkgs.rnnoise-plugin ];
 }
