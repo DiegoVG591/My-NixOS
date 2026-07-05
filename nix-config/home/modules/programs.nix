2450b7173ca6ecaaeae3a498add053dd363ad270
DiegoVG591 <diegovg591@gmail.com>
2026-07-05 15:27:26 +0200

    home: set zen as default browser via xdg.mimeApps

--- nix-config/home/modules/programs.nix
+++ nix-config/home/modules/programs.nix
@@ -25,6 +25,18 @@
     # --- ZEN BROWSER --- #
     programs.zen-browser.enable = true;
 
+    # --- SET ZEN AS DEFAULT BROWSER --- #
+    xdg.mimeApps = {
+        enable = true;
+        defaultApplications = {
+            "text/html"             = "zen.desktop";
+            "x-scheme-handler/http" = "zen.desktop";
+            "x-scheme-handler/https" = "zen.desktop";
+            "x-scheme-handler/about" = "zen.desktop";
+            "x-scheme-handler/unknown" = "zen.desktop";
+        };
+    };
+
     # --- HOME MANAGER --- #
     programs.home-manager.enable = true;
 }
